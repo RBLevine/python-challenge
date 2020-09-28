6c1a838ceec6b7f9f7605037f8c430475e2b33b9
Rebecca Levine <belen.levine17@gmail.com>
2020-09-27 21:07:39 -0600

    printing to terminal, not to .txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,16 @@
 import os
 import csv
 
-budgetData_csv = os.path.join("..", "Resources", "budget_data.csv")
+budgetData_csv = os.path.join( "Resources", "budget_data.csv")
 totalProLos=0
 totalMonths=0
 totalChange=0
 lastProLos=0
+recentChange=0
+greatProfit=0
+greatProfitMonth=""
+greatLoss=0
+greatLossMonth=""
 
 # Open and read csv
 with open(budgetData_csv) as csv_file:
@@ -13,7 +18,6 @@ with open(budgetData_csv) as csv_file:
 
     # Read the header row first (skip this part if there is no header)
     csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
 
     # Read through each row of data after the header
     for row in csv_reader:
@@ -21,12 +25,29 @@ with open(budgetData_csv) as csv_file:
         # Track total number of months in dataset
         totalMonths+=1
         # Track net total amount of Profit/Losses
-        totalProLos+=rows[1]
+        totalProLos+=int(row[1])
         # Track average change in Profit/Losses
-
+        if totalMonths==1:
+            lastProLos=int(row[1])
+        else:
+            recentChange=int(row[1])-lastProLos
+            totalChange+=recentChange
+            lastProLos=int(row[1])
         # Track greatest increase in profits (date and amount)
-
+        if recentChange>greatProfit:
+            greatProfit=recentChange
+            greatProfitMonth=str(row[0])           
         # Track greatest decrease in losses (date and amount)
-
-    
-    # Print out results to Terminal
+        elif recentChange<greatLoss:
+            greatLoss=recentChange
+            greatLossMonth=str(row[0]) 
+#calculate average change
+averageChange="%.2f" % (totalChange/(totalMonths-1))
+# Print out results to Terminal
+print("Financial Analysis")
+print("-------------------------")
+print(f"Total Month:{totalMonths}")
+print(f"Total: ${totalProLos}")
+print(f"Average Change: ${averageChange}")
+print(f"Greatest Increase in Profits: {greatProfitMonth} (${greatProfit})")
+print(f"Greatest Decrease in Profits: {greatLossMonth} (${greatLoss})")
