fb11585cd2188d28517b9285f028acd62abca247
Rebecca Levine <belen.levine17@gmail.com>
2020-10-04 20:51:41 -0600

    final comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,10 @@
 import os
 import csv
 
+# Path for the budget data file
 budgetData_csv = os.path.join( "Resources", "budget_data.csv")
+
+# Initializing all variables used in code below
 totalProLos=0
 totalMonths=0
 totalChange=0
@@ -29,27 +32,38 @@ with open(budgetData_csv) as csv_file:
         totalProLos+=int(row[1])
 
         # Track average change in Profit/Losses
+        # If we are on the first line, 
         if totalMonths==1:
+            # Set the lastProLos variable to the Profit/Loss data in that row
             lastProLos=int(row[1])
+        
+        # If we are not on the first line, 
         else:
+            # Calculate change between current line and previous line, 
             recentChange=int(row[1])-lastProLos
+            # Add that to the running change total, 
             totalChange+=recentChange
+            # Reset data for the lastProLos
             lastProLos=int(row[1])
        
         # Track greatest increase in profits (date and amount)
+        # If recentChange is greater than currently stored greatProfit
         if recentChange>greatProfit:
+            # Reset greatProfit to recentChange
             greatProfit=recentChange
+            # Store the month associated with the greatProfit/recentChange
             greatProfitMonth=str(row[0])     
                   
         # Track greatest decrease in losses (date and amount)
+        # If recent change is less than currently stored greatLoss
         elif recentChange<greatLoss:
+            # Reset greatLoss to recentChange
             greatLoss=recentChange
+            # Store the month associated with the greatLoss/recentChange
             greatLossMonth=str(row[0]) 
 
 #calculate average change
 averageChange="%.2f" % (totalChange/(totalMonths-1))
-# Print out results to Terminal
-
 
 # Specify the file to write to
 output_path = os.path.join("Analysis", "PyBankResults.txt")
@@ -57,8 +71,8 @@ output_path = os.path.join("Analysis", "PyBankResults.txt")
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, 'w') as txtfile:
 
-    print("Financial Analysis", file=txtfile) #print to the .txt
-    print("Financial Analysis") #print to terminal
+    print("Financial Analysis", file=txtfile) 
+    print("Financial Analysis") 
     print("-------------------------", file=txtfile)
     print("-------------------------")
     print(f"Total Months: {totalMonths}", file=txtfile)
